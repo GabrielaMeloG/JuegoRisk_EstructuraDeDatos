f70e0ffd4431d1113532dff17d1ff4e438c5613b
GabrielaMeloG <gabs1205@icloud.com>
2026-08-15 14:17:59 -0500

    Estructura inicial: juego.h

---
+++ juego.h
@@ -0,0 +1,40 @@
+#ifdef JUEGO_H
+#define JUEGO_H
+#include <string>
+#include <vector>
+#include <deque>
+
+//TAD jugador 
+struct Jugador {
+    std::string nombre;
+    std::string color;
+    bool obtenidoUnidades;
+    bool haAtacado; 
+};
+
+//TAD EstadoJuego
+class EstadoJuego {
+    public: 
+        EstadoJuego ();
+
+        bool inicializado;
+        bool terminado;
+
+        std::vector<Jugador> jugadores;
+        std::vector<Territorio> territorios;
+        std::deque<std::string> turnos;
+
+        bool existeJugador(std::string nombre);
+        int indiceJugador(std::string nombre);
+        bool esTurnoDe(std::string nombre);
+        std::string jugadorActual();
+        void siguienteTurno();
+
+        int indiceTerritorio(std::string codigo);
+
+        void reiniciar();
+
+        
+};
+#endif
+
